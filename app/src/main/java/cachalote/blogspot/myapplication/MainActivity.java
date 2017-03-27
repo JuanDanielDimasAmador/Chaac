85e0928d5bbaaedc5b1364e055493fe5445f9797
Raul Prior <1715110187@utec-tgo.edu.mx>
2017-03-26 22:58:06 -0500

    fisrt upload bluetooth

--- app/src/main/java/cachalote/blogspot/myapplication/MainActivity.java
+++ app/src/main/java/cachalote/blogspot/myapplication/MainActivity.java
@@ -1,8 +1,14 @@
 package cachalote.blogspot.myapplication;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +18,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = "Arduinio nano";
+    private Switch powerDevice;
+    private ImageView waterTankImage;
+    public String address =  "";
+    Handler bluetoothIn;
+    private int quantity;
+    private OutputStream outStream = null;
+    private InputStream inputStream =null;
+    final int handlerState = 0;
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private StringBuilder recDataString = new StringBuilder();
+    private TextView waterLevel;
+
+    // SPP UUID service - this should work for most devices
+    private static final UUID  MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +63,9 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                goToSearchDevice();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                  //      .setAction("Action", null).show();
             }
         });
 
@@ -40,7 +77,23 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        powerDevice = (Switch)findViewById(R.id.swich_power_device);
+        waterTankImage = (ImageView)findViewById(R.id.water_tank_imagen);
+        waterLevel = (TextView)findViewById(R.id.water_level);
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        Animation rise = (Animation) AnimationUtils.loadAnimation(this, R.anim.water_level);
+        waterLevel.setAnimation(rise);
+
+        if(getIntent().hasExtra("address")){
+            address = getIntent().getExtras().getString("address");
+        }
+        if(address.equals("")){
+            goToSearchDevice();
+        }
     }
+
 
     @Override
     public void onBackPressed() {
@@ -97,5 +150,120 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void getBluetoothData(){
+        bluetoothIn = new Handler() {
+            public void handleMessage(android.os.Message msg) {
+                if (msg.what == handlerState) {           //if message is what we want
+                    String readMessage = (String) msg.obj;// msg.arg1 = bytes from connect thread
+                    recDataString.append(readMessage);    //keep appending to string until ~
+                    int endOfLineIndex = recDataString.indexOf("~");// determine the end-of-line
+                    if (endOfLineIndex > 0) {                       // make sure there data before ~
+                        String dataInPrint = recDataString.substring(0, endOfLineIndex);// extract string
+                        Log.d("Get data from arduino", dataInPrint);//Message for logs
+                        quantity = Integer.parseInt(dataInPrint);   //Get quantity of water
+
+                        if(quantity >=75){
+                            waterTankImage.setImageResource(R.drawable.water_100);
+                            waterLevel.setText(""+quantity);
+                        }else if(quantity >= 50 && quantity <= 74){
+                            waterTankImage.setImageResource(R.drawable.water_75);
+                            waterLevel.setText(""+quantity);
+                        }else if(quantity >= 25 && quantity <= 49){
+                            waterTankImage.setImageResource(R.drawable.water_50);
+                            waterLevel.setText(""+quantity);
+                        }else if(quantity <= 24){
+                            waterTankImage.setImageResource(R.drawable.water_0);
+                            waterLevel.setText(""+quantity);
+                        }
+                        recDataString.delete(0, recDataString.length());//clear all string data
+                        dataInPrint = " ";
+                    }
+                }
+            }
+        };
+    }
+
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        System.out.println("---------------------------------->"+address);
+        BluetoothDevice device = btAdapter.getRemoteDevice(address);//Get MAC address from os
+        try {
+            btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+        } catch (IOException e) {
+            errorExit("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
+        }
+        btAdapter.cancelDiscovery();
+        Log.d(TAG, "Trying to connect");
+        try {
+            btSocket.connect();
+            Log.d(TAG, "Connectec and link opened");
+        } catch (IOException e) {
+            try {
+                btSocket.close();
+            } catch (IOException e2) {
+                errorExit("Fatal Error", "In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".");
+            }
+        }
+        try {
+            outStream = btSocket.getOutputStream();
+            inputStream = btSocket.getInputStream();
+        } catch (IOException e) {
+            errorExit("Fatal Error", "In onResume() and output stream creation failed:" + e.getMessage() + ".");
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "Get paused");
+
+        if (outStream != null) {
+            try {
+                outStream.flush();
+            } catch (IOException e) {
+                errorExit("Fatal Error", "In onPause() and failed to flush output stream: " + e.getMessage() + ".");
+            }
+        }
+        try     {
+            btSocket.close();
+        } catch (IOException e2) {
+            errorExit("Fatal Error", "In onPause() and failed to close socket." + e2.getMessage() + ".");
+        }
+    }
+
+    private void sendData(String message) {
+        byte[] msgBuffer = message.getBytes();
+        Log.d(TAG, "...Sending data: " + message + "...");
+        try {
+            outStream.write(msgBuffer);
+        } catch (IOException e) {
+            String msg = "In onResume() and an exception occurred during write: " + e.getMessage();
+
+            if (address.equals("00:00:00:00:00:00"))
+                msg = msg + ".\n\nUpdate your server address from 00:00:00:00:00:00 to the correct address on line 37 in the java code";
+            msg = msg +  ".\n\nCheck that the SPP UUID: " + MY_UUID.toString() + " exists on server.\n\n";
+
+            errorExit("Fatal Error", msg);
+        }
+    }
+
+    private void errorExit(String title, String message){
+        Toast msg = Toast.makeText(getBaseContext(), title + " - " + message, Toast.LENGTH_SHORT);
+        msg.show();
+        finish();
+    }
+
+    public void goToSearchDevice(){
+        Intent intent = new Intent(this, searchDevice.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void messageToast(String message){
+        Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
     }
 }
